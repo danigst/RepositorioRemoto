1a471b468fecaaa02def764791fb731cae293ece
daniel <daniel@macbook-air-de-daniel.home>
2024-03-09 00:36:57 +0100

    .

--- Proyecto/src/paquete2/clase3.java
+++ Proyecto/src/paquete2/clase3.java
@@ -5,6 +5,7 @@ public class clase3 {
 	public static void main(String[] args) {
 		System.out.println("Probando las ramas");
 		System.out.println("De la practica");
+		System.out.println("Prueba");
 
 	}
 
