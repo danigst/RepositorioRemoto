0b46a9f2665131ff061cc8a76b59cecec791248f
daniel <daniel@macbook-air-de-daniel.home>
2024-03-09 00:34:25 +0100

    ramas

---
+++ Proyecto/src/paquete2/clase3.java
@@ -0,0 +1,11 @@
+package paquete2;
+
+public class clase3 {
+
+	public static void main(String[] args) {
+		System.out.println("Probando las ramas");
+		System.out.println("De la practica");
+
+	}
+
+}
