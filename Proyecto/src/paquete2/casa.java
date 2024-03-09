bd0ad328aa74cd1bfd0bd1fb9b232543f70668f2
daniel <daniel@macbook-air-de-daniel.home>
2024-03-09 01:17:15 +0100

    commit 2

--- Proyecto/src/paquete2/casa.java
+++ Proyecto/src/paquete2/casa.java
@@ -5,6 +5,7 @@ public class casa {
 	public static void main(String[] args) {
 		System.out.println("Prueba de casa");
 		System.out.println("Para la practica");
+		System.out.println("Prueba 2");
 
 	}
 
